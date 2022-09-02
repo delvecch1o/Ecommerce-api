1605243af6e28ae981fd2f44db169cbd33a4848a
delvecch1o <rodrigopereirapinheiro@gmail.com>
2022-09-01 21:23:24 -0300

    login pronto

--- api/app/Http/Controllers/API/CadastrarController.php
+++ api/app/Http/Controllers/API/CadastrarController.php
@@ -61,12 +61,26 @@ class CadastrarController extends Controller
                     'message'=>'Credenciais Invalidas',
                 ]);
             } else{
-                $token = $user->createToken($user->email.'_Token')->plainTextToken;
+
+                if($user->role_as == 1 ) // 1 = Admin
+                { 
+                    $role = 'admin';
+                    $token = $user->createToken($user->email.'_AdminToken' , ['server:admin'])->plainTextToken;
+
+                } else{
+                    
+                    $role = '';
+                    $token = $user->createToken($user->email.'_Token', [''])->plainTextToken;
+
+                }
+
+                
                 return response()->json([
                     'status'=>200,
                     'username'=>$user->name,
                     'token'=>$token,
                     'message'=>'Login com Sucesso',
+                    'role'=>$role,
                 ]);
             }
 
